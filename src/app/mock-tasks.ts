e728228b65c12ad451779ea3abc2f967f9e6a847
Riju Dev <rijudev12@gmail.com>
2022-01-25 10:15:27 +0530

    commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import {Task} from './Task'
+export const TASKS = [
+    {
+        id: 1,
+        text: 'Doctor\'s Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting At School',
+        day: 'May 6th at 1.30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'may 7th  at 12.30pm',
+        reminder: false,
+    },
+];
